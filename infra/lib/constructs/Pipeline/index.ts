c17af392666e6a549952b0adb4c09d141c97525b
Ji Park <ji.park@jpnws.com>
2024-03-24 15:00:48 -0700

    update github source action

--- infra/lib/constructs/Pipeline/index.ts
+++ infra/lib/constructs/Pipeline/index.ts
@@ -192,8 +192,8 @@ export class PipelineStack extends Construct {
       actions: [
         new GitHubSourceAction({
           actionName: 'Source',
-          owner: 'westpoint-io',
-          repo: 'AWS-CDK-in-Action-Chapter-5',
+          owner: 'jpnws',
+          repo: 'cdk-studies5',
           branch: `${branch}`,
           oauthToken: secretToken,
           output: outputSource,
