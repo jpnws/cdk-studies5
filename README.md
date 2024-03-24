d4fb08ecd33f94b398fec2f69b35f141da4f7e07
Ji Park <ji.park@jpnws.com>
2024-03-24 10:44:46 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+# CDK Studies Chapter 5
+
+## Updating all packages
+
+- `npx npm-check-updates` -> `npx npm-check-updates -u`
+- or `pnpm update --latest`
+
+## Creating a CDK Project
+
+1. Create an empty folder.
+2. Create three folders: `infra`, `server`, `web`.
+3. Change into `infra` directory.
+4. Execute `cdk init --language=typescript`.
+
+## Common Commands
+
+### Infrastructure
+
+- `cdk deploy --profile cdk`
+- `cdk destroy --profile cdk`
+
+### Back-end
+
+- export AWS_PROFILE=cdk
+- export PORT=3001
---
+++ infra/utils/pipelineConfig.ts
@@ -0,0 +1,29 @@
+import * as dotenv from 'dotenv';
+
+export const pipelineConfig = (env: string) => {
+  if (env === 'Production') {
+    const { parsed } = dotenv.config({ path: '.env.production' });
+
+    return {
+      buildCommand: 'pnpm build:prod',
+      deployCommand: 'pnpm cdk deploy',
+      branch: 'main',
+      tag: 'chapter5-production-pipeline',
+      githubToken: parsed?.GITHUB_TOKEN,
+      workspaceId: parsed?.WORKSPACE_ID,
+      channelId: parsed?.CHANNEL_ID,
+    };
+  }
+
+  const { parsed } = dotenv.config({ path: '.env.development' });
+
+  return {
+    buildCommand: 'pnpm build:dev',
+    deployCommand: 'pnpm cdk:dev deploy',
+    branch: 'dev',
+    tag: 'chapter5-development-pipeline',
+    githubToken: parsed?.GITHUB_TOKEN,
+    workspaceId: parsed?.WORKSPACE_ID,
+    channelId: parsed?.CHANNEL_ID,
+  };
+};
